9d13489c8b66b6e4be1ab0836166ca0b85e9da48
Ghulam Muneer uddin <159724044+00Muneer12@users.noreply.github.com>
2024-10-12 10:47:57 +0500

    Add files via upload

---
+++ src/footer/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Footer() {
+  const route = useRouter()
+  return(
+    <div>
+      <h1>Welcome to my Footer Page</h1>
+
+      <ul>
+        <li><Link href='about'>About</Link></li>
+        <li><Link href='header'>Header</Link></li>
+        <li><Link href='navbar'>Navbar</Link></li>
+        {/* <li><Link href='footer'>Footer</Link></li> */}
+      </ul>
+
+        <ul>
+      <li><button className="p-5 bg-green-500" onClick={() => route.push('/about')}> About page</button></li>
+      <li><button className="p-5 bg-green-500" onClick={() => route.push('/header')}> Header page</button></li>
+      <li><button className="p-5 bg-green-500" onClick={() => route.push('/navbar')}> Navbar page</button></li>
+      {/* <li><button className="p-5 bg-green-500" onClick={() => route.push('/footer')}> Footer page</button></li> */}
+      </ul>
+    </div>
+
+  )
+}
